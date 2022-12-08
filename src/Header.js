52e951c05f0793cdb579b67f0fafa0e28e1ee473
岩渕晴暉 <105187244+iwabuchiharuki@users.noreply.github.com>
2022-12-08 09:45:17 +0900

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import SearchIcon from "@mui/icons-material/Search";
-import InputBase from "@mui/material/InputBase";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -9,51 +6,12 @@ import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import Typography from "@mui/material/Typography";
+import HomeIcon from "@mui/icons-material/Home";
+import Grid from "@mui/material/Grid";
 
 export default function MenuAppBar() {
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
-
-  {/*css要素*/}
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  }));
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "120%",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        width: "20ch",
-        "&:focus": {
-          width: "20ch",
-        },
-      },
-    },
-  }));
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -63,82 +21,58 @@ export default function MenuAppBar() {
     setAnchorEl(null);
   };
 
-  
-
-  /*検索バー　プロフィールアイコン */
+  /*ホームボタン　プロフィールアイコン */
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            おすすめ
-          </Typography>
-
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            カテゴリ
-          </Typography>
-
-          {/*Tabkey*/}
-          <Search>
-            <SearchIconWrapper>
-              {/*アイコン*/}
-            <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="teaching…"
-            />  
-          </Search>
-
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="coating…"
-            />
-          </Search>
-
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-            >
-                <AccountCircle />
+          {/*ホームボタン　aタグにリンク*/}
+          <Grid container sx={{ flexGrow: 1 }}>
+            <a href="home.html">
+              <IconButton>
+                <HomeIcon sx={{ fontSize: 40 }} color="black" />
               </IconButton>
-              {/*profileicon　位置*/}
-              <Menu
-                id="menu-appbar"
-                //anchorEl={//anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>
-                  <a href="Myaccount.html" target={"_blank"}>
-                    Profile
-                  </a>
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <a href="login.html" target={"_blank"}>
-                    Logout
-                  </a>
-                </MenuItem>
-              </Menu>
-            </div>
-          )}
+            </a>
+          </Grid>
+
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <Grid container sx={{ flexGrow: 1 }}>
+              <AccountCircle sx={{ fontSize: 40 }} color="black" />
+            </Grid>
+          </IconButton>
+
+          {/*profileiconのクリック後のメニュー位置*/}
+          <Menu
+            id="menu-appbar"
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <a href="Myaccount.html" target={"_blank"}>
+                Profile
+              </a>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <a href="login.html" target={"_blank"}>
+                Logout
+              </a>
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
