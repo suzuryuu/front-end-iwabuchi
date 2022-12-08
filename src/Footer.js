8dca043c469599d5d4ed10d0051bb3481efcef1b
岩渕晴暉 <105187244+iwabuchiharuki@users.noreply.github.com>
2022-12-08 11:09:47 +0900

    Add files via upload

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,14 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import {
+  FacebookShareButton,
+  LineShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  LineIcon,
+  TwitterIcon,
+} from "react-share";
 
 export default function Footer() {
   return (
@@ -20,7 +28,29 @@ export default function Footer() {
           >
             ©2022-team22
           </Typography>
-          
+
+          <Typography>
+            <FacebookShareButton
+              url={["シェアしたいURL"]}
+              quote={["シェア時に一緒に投稿したいテキスト"]}
+            >
+              <FacebookIcon round={true} />
+            </FacebookShareButton>
+
+            <LineShareButton
+              url={["シェアしたいURL"]}
+              quote={["シェア時に一緒に投稿したいテキスト"]}
+            >
+              <LineIcon round={true} />
+            </LineShareButton>
+
+            <TwitterShareButton
+              url={["シェアしたいURL"]}
+              quote={["シェア時に一緒に投稿したいテキスト"]}
+            >
+              <TwitterIcon round={true} />
+            </TwitterShareButton>
+          </Typography>
         </Toolbar>
       </AppBar>
     </Box>
