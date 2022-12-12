71b175d2a1dd41a3d91670c4519868c1ff897e1b
岩渕晴暉 <105187244+iwabuchiharuki@users.noreply.github.com>
2022-12-12 10:57:07 +0900

    Add files via upload

--- src/Matchinguser.js
+++ src/Matchinguser.js
@@ -1,8 +1,4 @@
-import React, { useState } from "react";
-import Avatar from "react-avatar-edit";
-import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
-import { InputText } from "primereact/inputtext";
 import img from "./user.jpg";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
@@ -10,50 +6,20 @@ import TextField from "@mui/material/TextField";
 import ReactStars from "react-rating-stars-component";
 
 export default function Matchinguser() {
-  /*プロフィール画像処理*/
-  const [imagecrop, setimagecrop] = useState("");
-  const [image, setimage] = useState("");
-  const [src, setsrc] = useState(false);
-  const [profile, setprofile] = useState([]);
-  const [pview, setpview] = useState(false);
-  const profileFinal = profile.map((item) => item.pview);
-
-  const onClose = () => {
-    setpview(null);
-  };
-
-  const onCrop = (view) => {
-    setpview(view);
-  };
-
-  /*受け取り画像処理*/
-  const saveCropImage = () => {
-    setprofile([...profile, { pview }]);
-    setimagecrop(false);
-  };
 
   /*星マークの受付*/
   const ratingChanged = (newRating) => {};
 
   return (
     <Box>
-      <Button 
-        variant="outlined" 
+      <Button
+        variant="outlined"
         color="primary"
         style={{
           width: "60px",
           height: "50px",
-              }}
-          label="戻る"
-      />
-
-      <InputText
-        type="file"
-        accept="/image/*"
-        style={{ display: "none" }}
-        onChange={(event) => {
-          const file = event.target.files[0];
         }}
+        label="戻る"
       />
 
       {/*全体位置指定*/}
@@ -64,40 +30,22 @@ export default function Matchinguser() {
         alignItems="center"
       >
         <Grid>
-          {/*画像位置*/}
+          {/*画像サイズ指定　初期画像は{img}user.jpg*/}
           <img
+            src={img}
             style={{
               width: "200px",
               height: "200px",
               borderRadius: "50%",
               objectFit: "cover",
             }}
-            onClick={() => setimagecrop(true)}
-            src={profileFinal.length ? profileFinal : img}
-            alt=""
           />
-
-          <Dialog
-            visible={imagecrop}
-            header={() => <p>プロフィール画像選択</p>}
-            onHide={() => setimagecrop(false)}
-          >
-            <Avatar
-              width={500}
-              height={400}
-              onCrop={onCrop}
-              onClose={onClose}
-              src={src}
-              shadingColor={"#474649"}
-              backgroundColor={"#474649"}
-            />
-
-            <Button onClick={saveCropImage} label="save" icon="pi pi-check" />
-          </Dialog>
         </Grid>
 
+        {/*マッチングしたユーザ名*/}
         <Grid>名前</Grid>
-
+        
+        {/*星マーク指定　星の数 星のサイズ*/}
         <Grid>
           評価
           <ReactStars
@@ -120,21 +68,21 @@ export default function Matchinguser() {
         </Grid>
 
         <Grid item xs={5} sm={8} pt={5}>
-        <Button
+          <Button
             style={{
-            width: "300px",
-            height: "50px",
-                  }}
+              width: "300px",
+              height: "50px",
+            }}
             label="コーチングリクエストを送る"
-        />
+          />
         </Grid>
 
         <Grid item xs={5} sm={8} pt={5}>
           <Button
-           style={{
-            width: "300px",
-            height: "50px",
-                  }}
+            style={{
+              width: "300px",
+              height: "50px",
+            }}
             label="保留する"
           />
         </Grid>
