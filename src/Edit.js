b27498d8f5be1e1dd90825d1b414378dc9629fb1
suzuryuu <te190751@gmail.com>
2022-12-19 16:35:42 +0900

    prePost

--- src/Edit.js
+++ src/Edit.js
@@ -13,6 +13,11 @@ import Checkbox from "@mui/material/Checkbox";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
+
+
+const baseURL = "https://w24fj3bk3k.execute-api.ap-northeast-1.amazonaws.com/deploy0_0";
+
+
 export default function Edit() {
   /*プロフィール画像処理*/
   const [imagecrop, setimagecrop] = useState("");
@@ -20,8 +25,9 @@ export default function Edit() {
   const [profile, setprofile] = useState([]);
   const [pview, setpview] = useState(false);
   const profileFinal = profile.map((item) => item.pview);
-  const baseURL = "https://w24fj3bk3k.execute-api.ap-northeast-1.amazonaws.com/deploy0_0";
-
+  const [post, setPost] = React.useState(null);
+  const data = new FormData();
+  
   const onClose = () => {
     setpview(null);
   };
@@ -47,14 +53,56 @@ export default function Edit() {
     { label: "League of Legends" },
   ];
 
-  const tag2 = [
-    { label: "ApexLegends" },
-    { label: "Splatoon3" },
-    { label: "Valolant" },
-    { label: "League of Legends" },
-  ];
   const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
   const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+  // 値を変更した時にvalueに一時保存
+  const [values, setValues] = React.useState({
+    nickname: '',
+    intro: '',
+    haveSkill: '',
+    wantSkill: ''
+  });
+
+  //nicknameの値を更新
+  const handleChange_nick = nickname => event => {
+    setValues({ ...values, [nickname]: event.target.value });
+  };
+
+  //introの値を更新
+  const handleChange_intro = intro => event => {
+    setValues({ ...values, [intro]: event.target.value });
+  };
+
+  //haveSkillの値の更新
+  const [inputValue_have, setInputValue_have] = React.useState('');
+
+  //wantSkillの値の更新
+  const [inputValue_want, setInputValue_want] = React.useState('');
+
+
+  // //　buttonを押すと呼ばれる
+  // //'Access-Control-Allow-Credentials' はAPIGatewayのCORS有効化時にtrueにしてます
+  const onClickGetAPI = async() => {
+    console.log(values)
+    const API_ENDPOINT = " https://2yk1rcryd1.execute-api.ap-northeast-1.amazonaws.com/profile_edit/edit"
+    const URL = API_ENDPOINT + values;
+    
+    try {
+        const response = await axios.post(URL,{
+          headers: {
+            'x-api-key': "RycZ63oLxu4wt31H0wPZc7cT0X6sqgEW3tso8pGI",
+            'Access-Control-Allow-Origin': 'http://localhost:3000'
+          }
+        });
+        console.log(response.data)
+        alert('リクエストを送信しました')
+    } catch (error) {
+        console.error(error)
+        alert('リクエスト処理に失敗しました')
+      }
+    }
+
 
   return (
     <Box>
@@ -116,6 +164,7 @@ export default function Edit() {
             placeholder="name"
             multiline
             variant="standard"
+            onChange={handleChange_nick('nickname')}
           />
         </Grid>
 
@@ -126,35 +175,54 @@ export default function Edit() {
             placeholder="hello!!"
             multiline
             variant="standard"
+            onChange={handleChange_intro('intro')}
           />
         </Grid>
 
         <Grid item xs={5} sm={8} pt={5}>
           教えたい技術
+          <div>{`inputValue_have: '${inputValue_have}'`}</div>
           <Autocomplete
             disablePortal
             id="combo-box-demo"
             options={tag}
             sx={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="Teaching" />}
+            //onInputChangeでvaluesにhaveSkillの値を一時的に保存する
+            inputValue_have={inputValue_have}
+            value={values.haveSkill}
+            onInputChange={(event, newInputValue) => {
+              console.log(newInputValue)
+              setInputValue_have(newInputValue)
+              setValues({ ...values, haveSkill: newInputValue});
+            }}
           />
         </Grid>
 
         
         <Grid item xs={5} sm={8} pt={5}>
-          教えたい技術
+          教わりたい技術
           <Autocomplete
             disablePortal
-            id="combo-box-demo"
+            id="combo-box-demo2"
             options={tag}
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Teaching" />}
+            renderInput={(params) => <TextField {...params} label="coaching" />}
+            //onInputChangeでvaluesにwantSkillの値を一時的に保存する
+            inputValue_want={inputValue_want}
+            value={values.wantSkill}
+            onInputChange={(event, newInputValue) => {
+              console.log(newInputValue)
+              setInputValue_want(newInputValue)
+              setValues({...values, wantSkill: newInputValue});
+            }}
           />
         </Grid>
         
 
         <Grid item xs={5} sm={8} pt={5}>
           <Button
+            onClick={onClickGetAPI}
             variant="outlined"
             color="primary"
             style={{
