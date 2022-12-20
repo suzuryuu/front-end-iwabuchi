ff7d41e38bf309cefd26cf2b1f1836cbbe7ea530
岩渕晴暉 <105187244+iwabuchiharuki@users.noreply.github.com>
2022-12-20 11:39:30 +0900

    Add files via upload

--- src/title.js
+++ src/title.js
@@ -1,6 +1,7 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import './title.css';
 
 
 export default function free() {
@@ -10,13 +11,13 @@ export default function free() {
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={50}>
           <Grid item xs={12} align="center">
-            <Typography variant="h2">gameskillcoting</Typography>
+            <Typography variant="h2"  class="text1" >Game Skill TeO</Typography>
           </Grid>
           <Grid item xs={12} align="center">
-          <Typography variant="h2">互いにゲームスキルをUP!</Typography>
+          <Typography variant="h2"class="text1" >Mutual gaming skill UP!</Typography>
           </Grid>
-          <Grid item xs={12} align="center">
-          <Typography variant="h2">利用手順</Typography>
+          <Grid item xs={12} align="center" >
+          <Typography variant="h2" class="text1" >利用手順</Typography>
           </Grid>
         </Grid>
       </Box>
