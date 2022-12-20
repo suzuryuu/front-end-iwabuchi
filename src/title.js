a48dd41db3db869f3aff14f988097569e1b57ea9
岩渕晴暉 <105187244+iwabuchiharuki@users.noreply.github.com>
2022-12-20 13:32:56 +0900

    Add files via upload

--- src/title.js
+++ src/title.js
@@ -14,7 +14,7 @@ export default function free() {
             <Typography variant="h2"  class="text1" >Game Skill TeO</Typography>
           </Grid>
           <Grid item xs={12} align="center">
-          <Typography variant="h2"class="text1" >Mutual gaming skill UP!</Typography>
+          <Typography variant="h1"class="text1" >Game</Typography>
           </Grid>
           <Grid item xs={12} align="center" >
           <Typography variant="h2" class="text1" >利用手順</Typography>
