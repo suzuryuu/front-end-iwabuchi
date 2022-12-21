d11f02710f9c0b91fd53c69362b06ca87da359e8
岩渕晴暉 <105187244+iwabuchiharuki@users.noreply.github.com>
2022-12-21 09:08:50 +0900

    Add files via upload

---
+++ src/titleunder.js
@@ -0,0 +1,73 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import './title.css';
+
+export default function titleunder() {
+  return (
+    <Box>
+      {/*カードサイズ sx={{ maxWidth: 300 }} */}
+      <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}  class="back">
+        <CardContent>
+          {/*カードのヘッダー*/}
+          <CardHeader
+            title="新規登録"
+            titleTypographyProps={{variant:'h3' }}
+            align="center"
+            style={{ backgroundColor: "gray", color: "white"}}
+            class="text1"
+          />
+          <Typography gutterBottom variant="h5" component="div" align="center">
+            <a href="logon.html">登録はこちらから</a>
+          </Typography>
+
+          <Typography color="text.secondary"align="center" style={{color: "red"}}>
+            ＊メールアドレスが必要になります。
+          </Typography>
+        </CardContent>
+
+        <Grid spacing={2}>
+          <CardContent>
+            <CardHeader
+              title="ログイン"
+              titleTypographyProps={{variant:'h3' }}
+              align="center"
+              style={{ backgroundColor: "gray", color: "white"  }}
+              class="text1"
+            />
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              align="center"
+            >
+              <a href="login.html">登録済みの方はこちらから</a>
+            </Typography>
+            <Typography color="text.secondary"align="center" style={{color: "red"}}>
+            ＊メールアドレス、パスワードが必要になります。
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <CardContent>
+          <CardHeader
+            title="退会"
+            titleTypographyProps={{variant:'h3' }}
+            align="center"
+            style={{ backgroundColor: "gray" , color: "white" }}
+            class="text1"
+          />
+          <Typography gutterBottom variant="h5" component="div" align="center">
+          <a href="login.html">退会はこちらから</a>
+          </Typography>
+          <Typography color="text.secondary"align="center" style={{color: "red"}}>
+            ＊退会するとアカウントが消えます。
+            </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+}
